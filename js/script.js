6925059536ded55695122c56293ada7b184cb7ba
Thapa <thapab@leidos.com>
2021-08-19 15:29:22 -0400

    adding files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
+
+  $('.nav-button').click(function() {
+    $('.nav-button').toggleClass('change');
+  });
+
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if(position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+ 
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
